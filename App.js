5d4598e0d20f7a5b5c895274f1a9c5b319d646ad
Konain1 <kaunainkasab@gmail.com>
2025-03-01 23:18:46 +0530

    first react-native done

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 // import { StatusBar } from 'expo-status-bar'
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import {
   Button,
   Pressable,
@@ -9,102 +9,109 @@ import {
   Text,
   TextInput,
   View,
-  SafeAreaView
+  SafeAreaView,
+  Platform
 } from 'react-native'
 
-import GoalItems from './components/GoalItems';
+import GoalItems from './components/GoalItems'
 import GoalInput from './components/GoalInput'
-
-
+import { StatusBar } from 'expo-status-bar'
 
 export default function App () {
   const [enteredText, setEnteredText] = useState('')
   const [goalLists, setGoalLists] = useState([])
-  const [showModal,setShowModal]=useState(false)
-  
-  
-    const handleEnterText = text => {
-      setEnteredText(text)
-    }
-    const handleGoalLists = () => {
-      if(enteredText == ''){
-        alert("please enter something!1")
-        return;
-      }
-      setGoalLists(prevGoals => [
-        ...prevGoals,
-        { text: enteredText, id: Math.random().toString() }
-      ])
-      setEnteredText('')
-      setShowModal(false)
-    }
-    const handlerDeleteItem = (id)=>{
+  const [showModal, setShowModal] = useState(false)
 
+  const handleEnterText = text => {
+    setEnteredText(text)
+  }
+  const handleGoalLists = () => {
+    if (enteredText == '') {
+      alert('please enter something!1')
+      return
+    }
+    setGoalLists(prevGoals => [
+      ...prevGoals,
+      { text: enteredText, id: Math.random().toString() }
+    ])
+    setEnteredText('')
+    setShowModal(false)
+  }
+  const handlerDeleteItem = id => {
+    setGoalLists(currentGola => currentGola.filter(item => item.id != id))
+    console.log(goalLists)
+  }
 
-      setGoalLists(currentGola=> currentGola.filter(item=>item.id != id))
-      console.log(goalLists)
-     
-    }
+  const handleShowModal = () => {
+    setShowModal(true)
 
-    const handleShowModal=()=>{
-      setShowModal(true)
-     
-      console.log('pressed')
-    }
-    const handleShowModalCancel =()=>{
-      setShowModal(false)
-    }
+    console.log('pressed')
+  }
+  const handleShowModalCancel = () => {
+    setShowModal(false)
+  }
 
   return (
+    <>
+
+   <StatusBar  style={Platform.OS == 'android' ? 'light'  : 'Dark'}/>
     <SafeAreaView style={styles.mainContainer}>
-    <View style={styles.setgoal}>
+      <View style={styles.setgoal}>
+        <Button title='set goals' onPress={handleShowModal} />
+      </View>
 
-     <Button  title='set goals' onPress={handleShowModal} />
-    </View>
-  
-      <GoalInput onCancelModal={handleShowModalCancel} showModal={showModal} handleEnterText={handleEnterText} handleGoalLists={handleGoalLists} enteredText={enteredText}/>
+      <GoalInput
+        onCancelModal={handleShowModalCancel}
+        showModal={showModal}
+        handleEnterText={handleEnterText}
+        handleGoalLists={handleGoalLists}
+        enteredText={enteredText}
+      />
 
-   
       <View style={styles.goalContainer}>
         <Text style={{ padding: 10 }}>Goal lists....</Text>
 
         <FlatList
           data={goalLists}
           renderItem={itemData => (
-            <GoalItems  id={itemData.item.id} handlerDeleteItem={handlerDeleteItem } text={itemData.item.text} />
-          )} 
-          keyExtractor={(item,key)=>{return item.id}}
+            <GoalItems
+              id={itemData.item.id}
+              handlerDeleteItem={handlerDeleteItem}
+              text={itemData.item.text}
+            />
+          )}
+          keyExtractor={(item, key) => {
+            return item.id
+          }}
         />
-         
-        
       </View>
     </SafeAreaView>
+    </>
   )
 }
 
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
-    margin:40,
-   alignItems:'center',
+    alignItems: 'center',
+    marginTop: 40
   },
- 
+
   goalContainer: {
     flex: 1,
     backgroundColor: '#dadada',
     borderRadius: 10,
-   width:'100%',
+    width: '100%'
   },
-  
+
   addbtn: {
     borderRadius: 10,
     backgroundColor: 'red',
     fontSize: 100
   },
-setgoal:{
-  margin:20,
-  width:200,
-  alignItems:'center',
-
-}
+  setgoal: {
+    margin: 20,
+    width: 200,
+    alignItems: 'center'
+  }
 })
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,29 +1,45 @@
-import { StyleSheet, View, Text, TextInput, Button, Modal ,Pressable} from 'react-native'
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Button,
+  Modal,
+  Pressable,
+  Image
+} from 'react-native'
 
 import { useState } from 'react'
 
 function GaolInput (props) {
   return (
-    <Modal visible={props.showModal} animationType='slide' onRequestClose={false} >
-
-  
-    <View style={styles.container}>
-      <TextInput
-        style={styles.textInput}
-        placeholder='enter here'
-        value={props.enteredText}
-        onChangeText={props.handleEnterText}
-      />
-      {/* <Pressable> <Text style={{fontSize:18,color:'blue'}}>submit </Text></Pressable> */}
-      <View style={styles.btnContainer}>
-      <Button
-        title='add goal'
-        onPress={props.handleGoalLists}
-      />
-      <Button title='cancel' color='red' onPress={props.onCancelModal} />
+    <Modal
+      visible={props.showModal}
+      animationType='slide'
+      onRequestClose={false}
+    >
+      <View style={styles.container}>
+        <Image
+          style={styles.img}
+          source={require('../assets/images/goal.png')}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder='enter here'
+          value={props.enteredText}
+          onChangeText={props.handleEnterText}
+        />
+        {/* <Pressable> <Text style={{fontSize:18,color:'blue'}}>submit </Text></Pressable> */}
+        <View style={styles.btnContainer}>
+          <Pressable onPress={props.handleGoalLists}>
+            <Text style={styles.addText}> Add Goal</Text>
+          </Pressable>
+          <Pressable onPress={props.onCancelModal}>
+         
+            <Text style={styles.cancelText}>Cancel</Text>
+          </Pressable>
+        </View>
       </View>
-     
-    </View>
     </Modal>
   )
 }
@@ -31,24 +47,39 @@ function GaolInput (props) {
 export default GaolInput
 
 const styles = StyleSheet.create({
-
   container: {
-    flex:1,
-    backgroundColor: '#fff',
+    flex: 1,
+    backgroundColor: '#311b6b',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   textInput: {
     borderWidth: 1,
-    borderColor: 'green',
+    borderColor: '#316adb',
+    backgroundColor:'#967adb',
     padding: 5,
     width: '60%',
-    borderRadius: 10
+    borderRadius: 10,
+    color: 'white'
   },
-  btnContainer:{
-    flexDirection:'row',
-    justifyContent:'center',
-    alignItems:'center'
+  btnContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    width: '60%',
+    margin: 5
+  },
+  img: {
+    width: 100,
+    height: 100,
+    margin: 20
+  },
+  addText: {
+    fontSize: 18,
+    color: 'green'
+  },
+  cancelText: {
+    fontSize: 18,
+    color: 'red'
   }
-
 })
